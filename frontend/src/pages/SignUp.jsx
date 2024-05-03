0033016708fb23a5146b44661c7f5349175aa86d
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-05-03 20:21:53 +0530

    create sign-up page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>
-      
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold
+      my-7 text-blue-200'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='username' className='border p-3 rounded-lg bg-blue-200' id='username' />
+        <input type="email" placeholder='email' className='border p-3 rounded-lg bg-blue-200' id='email' />
+        <input type="password" placeholder='password' className='border p-3 rounded-lg bg-blue-200' id='password' />
+        <button className=' bg-blue-800 text-white p-3 rounded-lg uppercase hover:opacity-90'>Sign Up</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p className='text-white font-semibold'>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-blue-300 font-bold'>Sign in</span>
+        </Link>
+      </div>
+
     </div>
   )
 }
