160e3bd137f9ad6fc773fb5765bba117a0368586
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-05-13 23:33:35 +0530

    create listing page completed

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
+import { FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair } from 'react-icons/fa';
 
 export default function Listing() {
 
@@ -11,6 +12,7 @@ export default function Listing() {
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [copied, setCopied] = useState(false);
     const params = useParams();
     useEffect(() => {
         const fetchListing = async () => {
@@ -47,16 +49,79 @@ export default function Listing() {
                         {listing.imageUrls.map((url) => (<SwiperSlide key={url}>
                             <div className='h-[450px]' style={{
                                 background: `url(${url})
-                                   center no-repeat`, backgroundSize: 'cover'}}>
+                                   center no-repeat`, backgroundSize: 'cover'
+                            }}>
 
                             </div>
                         </SwiperSlide>
                         )
                         )}
                     </Swiper>
+                    <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-blue-100 cursor-pointer'>
+                        <FaShare
+                            className='text-blue-400'
+                            onClick={() => {
+                                navigator.clipboard.writeText(window.location.href);
+                                setCopied(true);
+                                setTimeout(() => {
+                                    setCopied(false);
+                                }, 2000);
+                            }} />
+
+                    </div>
+                    {copied && (
+                        <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-blue-100 p-2'>
+                            Link copied!
+                        </p>
+                    )}
+                    <div className='flex flex-col max-w-4xl mx-auto p-3 my-7'>
+                        <p className='text-2xl font-semibold'>
+                            {listing.name} - ${' '}
+                            {listing.offer ? listing.discountPrice.toLocaleString('en-US') : listing.regularPrice.toLocaleString('en-US')}
+                            {listing.type === 'rent' && ' / month'}
+                        </p>
+                        <p className='flex items-center mt-6 gap-2 text-blue-400 my-2 text-sm'>
+                            <FaMapMarkerAlt className='text-green-500' />
+                            {listing.address}
+                        </p>
+                        <div className='flex gap-4'>
+                            <p className='bg-red-700 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                                {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
+                            </p>
+                            {
+                                listing.offer && (
+                                    <p className='bg-green-400 w-full max-w-[200px] text-white text-center p-1 rounded-md'>${+listing.regularPrice - +listing.discountPrice}</p>
+                                )
+                            }
+                        </div>
+
+                        <p className='text-blue-200'>
+                            <span className='font-semibold text-white'>
+                                Description - {' '}
+                            </span>
+                            {listing.description}
+                        </p>
+                        <ul className='text-green-500 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+                            <li className='flex items-center gap-1 whitespace-nowrap '>
+                                <FaBed className='text-lg' />
+                                {listing.bedrooms > 1 ? `${listing.bedrooms} beds` : `${listing.bedrooms} bed`}
+                            </li>
+                            <li className='flex items-center gap-1 whitespace-nowrap '>
+                                <FaBath className='text-lg' />
+                                {listing.bathrooms > 1 ? `${listing.bathrooms} bath` : `${listing.bathrooms} bath`}
+                            </li>
+                            <li className='flex items-center gap-1 whitespace-nowrap '>
+                                <FaParking className='text-lg' />
+                                {listing.parking ? 'Parking' : 'No parking'}
+                            </li>
+                            <li className='flex items-center gap-1 whitespace-nowrap '>
+                                <FaChair className='text-lg' />
+                                {listing.furnished ? 'Furnished' : 'Unfurnished'}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            )
-            }
+            )}
         </main>
     )
 }
